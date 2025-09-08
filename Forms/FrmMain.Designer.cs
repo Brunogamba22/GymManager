b90ac85ce3793540019d9d3c7fcd3a2ce7471431
JonathanGarcia <192479135+JonathanGarciaMartin10@users.noreply.github.com>
2025-09-08 19:30:19 -0300

    Actualizo FrmMain (diseñador) y proyecto

--- Forms/FrmMain.Designer.cs
+++ Forms/FrmMain.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelFooter = new System.Windows.Forms.Panel();
+            this.panelNavbar = new System.Windows.Forms.Panel();
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(800, 60);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // panelFooter
+            // 
+            this.panelFooter.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panelFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelFooter.Location = new System.Drawing.Point(0, 420);
+            this.panelFooter.Name = "panelFooter";
+            this.panelFooter.Size = new System.Drawing.Size(800, 30);
+            this.panelFooter.TabIndex = 1;
+            // 
+            // panelNavbar
+            // 
+            this.panelNavbar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panelNavbar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelNavbar.Location = new System.Drawing.Point(0, 60);
+            this.panelNavbar.Name = "panelNavbar";
+            this.panelNavbar.Size = new System.Drawing.Size(200, 360);
+            this.panelNavbar.TabIndex = 2;
+            // 
+            // panelContenido
+            // 
+            this.panelContenido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenido.Location = new System.Drawing.Point(200, 60);
+            this.panelContenido.Name = "panelContenido";
+            this.panelContenido.Size = new System.Drawing.Size(600, 360);
+            this.panelContenido.TabIndex = 3;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmMain";
+            this.Controls.Add(this.panelContenido);
+            this.Controls.Add(this.panelNavbar);
+            this.Controls.Add(this.panelFooter);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GymManager";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelFooter;
+        private System.Windows.Forms.Panel panelNavbar;
+        private System.Windows.Forms.Panel panelContenido;
     }
 }
