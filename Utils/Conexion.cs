584b80240503ac002997b703ad17703e89c25acf
Brunogamba22 <brunogamba439@gmail.com>
2025-09-20 20:04:18 -0300

    primeros pasos con la base de datos

---
+++ Utils/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Configuration;
+
+namespace GymManager.Utils
+{
+    // Clase estática para acceder a la cadena de conexión desde cualquier parte del proyecto
+    public static class Conexion
+    {
+        // Propiedad de solo lectura que obtiene la cadena de conexión desde App.config
+        public static string CadenaConexion =>
+            ConfigurationManager.ConnectionStrings["GymDB"].ConnectionString;
+    }
+}
